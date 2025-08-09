d4678e8fd26b89f274ad4427cba4fcb938bc1555
Mohammad Dayarneh <m.daya.nutrino@gmail.com>
2025-08-09 20:33:00 +0400

    update

--- db/database.py
+++ db/database.py
@@ -5,6 +5,7 @@ from core.config import settings
 # Initialize the client directly.
 # If the MONGO_DETAILS are invalid, this will raise an error and crash the app on startup,
 # which is the correct behavior. This prevents the app from running without a database.
+print(f"settings.MONGO_DETAILS : {settings.MONGO_DETAILS}")
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_DETAILS)
 
 # Get a reference to the database
